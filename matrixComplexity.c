4a4408a0dcb56c69896d3e3d61021289fa38cec4
Dylan Jara <dylan.jara@usach.cl>
2024-11-15 13:03:54 -0300

    Avance matrixComplexity

---
+++ matrixComplexity.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 10
+#define M N*N
+
+int matrixMultiplication(int dim, int A[dim][dim], int B[dim][dim]);
+void matrixReading(int C[M][M]);
+
+int main(){
+    int dim;
+    printf("matrix complexity: ");
+    return 0;
+}
+
+int matrixMultiplication(int dim, int A[dim][dim], int B[dim][dim]){
+    int i, j;
+    for(i = 0; i < dim; i++){
+        for(j = 0; j < dim; j++){
+            
+        }
+    }
+};
