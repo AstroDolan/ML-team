cdedbf7e6870b678ae2d709dcca2b4ac7fa8d3ec
Dylan Jara <djaracarvajal@gmail.com>
2024-11-15 11:06:33 -0300

    m2c y matrixGen funcionales
    m2c y matrixGen funcionan sin problemas. Se debe investigar ahora su
    optimizacion y cumplir con las tareas especificadas en el documento
    del profesor

--- matrixGen.c
+++ matrixGen.c
@@ -1,37 +1,50 @@
+#include<stdlib.h>
 #include<stdio.h>
 #include<time.h>
+
+void fillMatrix(int dim, int maze[dim][dim]);
+void showMatrix(int dim, int maze[dim][dim]);
 
 int main(){
     srand(time(NULL));
     int dim;
 
-    printf("Ingrese la dimension: ");
-    scanf("d",&dim);
+    printf("Ingrese la dimension: \n");
+    scanf("%d",&dim);
     int maze[dim][dim];
 
-    showMatrix(dim, maze);
     fillMatrix(dim, maze);
+    showMatrix(dim, maze);
+    
 
     return 0;
 }
 
-void showMatrix(int dim, int maze[dim][dim]){
-    int i, j;
-    for(i = 0; i < dim; i++){
-        for(j = 0; i < dim; i++){
-            printf("d", maze[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-
 void fillMatrix(int dim, int maze[dim][dim]){
     int i, j;
     for(i = 0; i < dim; i++){
-        for(j = 0; i < dim; i++){
-            maze[dim][dim] = 1 + rand() % 9;
+        for(j = 0; j < dim; j++){
+            maze[i][j] = 1 + rand() % (dim-1);
         }
     }
 }
 
+void showMatrix(int dim, int maze[dim][dim]){
+    int i, j;
+    printf("int maze[%d][%d] = \n{\n", dim, dim);
+    for(i = 0; i < dim; i++){
+        printf("   { ");
+        for(j = 0; j < dim; j++){
+            printf("%d", maze[i][j]);
+            if(j < dim - 1){
+                printf(", ");
+            }
+        }
+        printf(" }");
+        if(i < dim - 1){
+            printf(",");
+        }
+        printf("\n");
+    }
+    printf("};\n");
+}
