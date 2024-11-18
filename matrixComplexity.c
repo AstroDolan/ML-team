8e3a229ad4802bdc98cc5244c2bac95b4712f62b
Dylan Jara <dylan.jara@usach.cl>
2024-11-18 00:16:50 -0300

    Updated

--- matrixComplexity.c
+++ matrixComplexity.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define N 10
-#define M N*N
+#define N 4
 
 /*
 Este trabajo consta de multiplicar matrices de distintas formas
@@ -10,31 +8,48 @@ Primero con i,j luego j,i
 Doble punteros, etc.
 */
 
-int matrixMulti_rowcol(int dim, int A[dim][dim], int B[dim][dim]);
-int matrixMulti_colrow(int dim, int A[dim][dim], int B[dim][dim]);
-void matrixReading(int C[M][M]);
+int matrixMulti_rowcol(int dim, int A[dim][dim], int B[dim][dim], int C[dim][dim]);
+int matrixMulti_colrow(int dim, int A[dim][dim], int B[dim][dim], int C[dim][dim]);
+void print_rowcol(int dim, int C[dim][dim]);
+void print_colrow(int dim, int C[dim][dim]);
+
+int matrixMulti_pointer(int dim, int *A, int *B, int *C);
+void print_pointer(int dim, int *C);
 
 int main(){
-    int dim = 3; //Esto depende de mis matrices de entrada en el main
+    
+    int dim = N; //Esto depende de mis matrices de entrada en el main
 
-    int A[dim][dim] = {
+    int A[N][N] = {
         {4, 6, 9, 2},
         {1, 6, 8, 2},
         {2, 5 ,9, 4},
         {1, 1, 7, 5}
-    }
+    };
 
-    int B[dim][dim] = {
+    int B[N][N] = {
         {3, 6, 9, 1},
         {2, 2, 1, 8},
         {9, 7, 5, 2},
         {1, 7, 6, 5}
-    }
-    printf("matrix complexity: ");
+    };
+
+    int C[dim][dim];
+
+    matrixMulti_rowcol(dim, A, B, C);
+    print_rowcol(dim, C);
+
+    matrixMulti_colrow(dim, A, B, C);
+    print_colrow(dim, C);
+
+    matrixMulti_pointer(dim, *A, *B, *C);
+    print_pointer(dim, *C);
+    
     return 0;
 }
 
 int matrixMulti_rowcol(int dim, int A[dim][dim], int B[dim][dim], int C[dim][dim]){
+    int i, j, k;
     for(i = 0; i < dim; i++){
         for(j = 0; j < dim; j++){
             C[i][j] = 0; //Inicializo la matriz
@@ -43,25 +58,49 @@ int matrixMulti_rowcol(int dim, int A[dim][dim], int B[dim][dim], int C[dim][dim
             }
         }
     }
-};
+}
 
-void print_rowcol(int dim, int C[i][j]){
-    printf("Matriz resultante: ");
+int matrixMulti_colrow(int dim, int A[dim][dim], int B[dim][dim], int C[dim][dim]){
+    int i, j, k;
     for(i = 0; i < dim; i++){
         for(j = 0; j < dim; j++){
-            printf("%d", C[i][j]);
-            if(j < dim - 1){
-                printf("\n");
+            C[i][j] = 0; //Inicializo la matriz
+            for(k = 0; k < dim; k++){
+                C[i][j] += A[j][k] * B[k][i]; //Asigno las multiplicaciones a la matriz C
             }
         }
     }
 }
 
-int matrixMulti_rowcol(int dim, int A[dim][dim], int B[dim][dim]){
-    int i, j;
-    for(j = 0; j < dim; j++){
-        for(i = 0; i < dim; i++){
-            printf("pao hermosa");
+int matrixMulti_pointer(int dim, int *A, int *B, int *C){
+    int i, j, k;
+    
+    for(i = 0; i < dim; i++){
+        for(j = 0; j < dim; j++){
+            *(C + i * dim + j) = 0; //Se inicializa C con solo ceros    
         }
     }
-};
+}
+
+
+void print_rowcol(int dim, int C[dim][dim]){
+    int i, j;
+    printf("\nMatriz resultante de filas-columnas: \n");
+    for(i = 0; i < dim; i++){
+        for(j = 0; j < dim; j++){
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void print_colrow(int dim, int C[dim][dim]){
+    int i, j;
+    printf("\nMatriz resultante de columnas-filas: \n");
+    for(i = 0; i < dim; i++){
+        for(j = 0; j < dim; j++){
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+}
--- matrixcomp.c
+++ matrixcomp.c
@@ -1,48 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void multiplicarMatrices(int n, int matriz1[n][n], int matriz2[n][n], int resultado[n][n]) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            resultado[i][j] = 0;
-            for (int k = 0; k < n; k++) {
-                resultado[i][j] += matriz1[i][k] * matriz2[k][j];
+// Función para multiplicar matrices cuadradas usando punteros
+void multiply_matrices(int *A, int *B, int *C, int dim) {
+    int i, j, k;
+
+    // Inicializar la matriz resultado C con ceros
+    for (i = 0; i < dim; i++) {
+        for (j = 0; j < dim; j++) {
+            *(C + i * dim + j) = 0;
+        }
+    }
+
+    // Multiplicar A y B, y almacenar el resultado en C
+    for (i = 0; i < dim; i++) {
+        for (j = 0; j < dim; j++) {
+            for (k = 0; k < dim; k++) {
+                *(C + i * dim + j) += *(A + i * dim + k) * *(B + k * dim + j);
             }
         }
     }
 }
 
-void imprimirMatriz(int n, int matriz[n][n], const char *nombre) {
-    printf("Matriz %s:\n", nombre);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%d ", matriz[i][j]);
+// Función para imprimir una matriz cuadrada representada como un puntero
+void print_matrix(int *M, int dim) {
+    int i, j;
+    for (i = 0; i < dim; i++) {
+        for (j = 0; j < dim; j++) {
+            printf("%d ", *(M + i * dim + j));
         }
         printf("\n");
     }
 }
 
 int main() {
-    // Declaración de matrices directamente en el código
-    int n = 3;  // Tamaño de la matriz
-    int matriz1[3][3] = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
-    int matriz2[3][3] = {
-        {9, 8, 7},
-        {6, 5, 4},
-        {3, 2, 1}
-    };
-    int resultado[3][3];
+    int dim = 4;
 
-    // Llamada a la función de multiplicación
-    multiplicarMatrices(n, matriz1, matriz2, resultado);
+    // Declaración de matrices como arreglos unidimensionales
+    int A[] = {
+        4, 6, 9, 2,
+        1, 6, 8, 2,
+        2, 5, 9, 4,
+        1, 1, 7, 5
+    };
 
-    // Impresión de resultados
-    imprimirMatriz(n, matriz1, "A");
-    imprimirMatriz(n, matriz2, "B");
-    imprimirMatriz(n, resultado, "Resultado");
+    int B[] = {
+        3, 6, 9, 1,
+        2, 2, 1, 8,
+        9, 7, 5, 2,
+        1, 7, 6, 5
+    };
+
+    int C[dim * dim]; // Matriz resultado
+
+    // Llamar a la función para multiplicar matrices
+    multiply_matrices(A, B, C, dim);
+
+    // Imprimir la matriz resultado
+    printf("Matriz resultante:\n");
+    print_matrix(C, dim);
 
     return 0;
 }
