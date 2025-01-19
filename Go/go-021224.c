fe4c4b20b3f9b48e7d7624357f8f960b77f07cb2
Dylan Jara <djaracarvajal@gmail.com>
2025-01-19 10:40:33 -0300

    Changed PINN

--- Go/go-021224.c
+++ Go/go-021224.c
@@ -1,18 +1,5 @@
-/******************************************************************************
- *
- * go-021224.c
- * 
- * Programmer: Ruben Carvajal Schiaffino
- *
- * Santiago de Chile, 2/12/2024
- *
- ******************************************************************************/
- 
-
 #include <stdio.h>
 #include <stdlib.h>
-
-
 #define EMPTY     0
 #define NORMAL    1
 #define FULL      2
@@ -29,38 +16,24 @@
 #define SIMBOLIC  'S'
 #define TEXTUAL   'T'
 
-
 struct GOCell {
-
    unsigned char element, statuscell, prop;
 };       
 
 
 struct Queue {
-
    unsigned int *memarea, begin, end, qsize;
 };  
 
-
-/*
- * Initializes a Queue
- */
 struct Queue *InitQueue(unsigned int size) {
-
    struct Queue *q;
-
    q = calloc(1,sizeof(struct Queue));
    q->memarea = calloc(size,sizeof(unsigned int));
    q->qsize = size;
    return q;
 }
 
-
-/*
- * Verifies the queue status
- */
 unsigned char StatusQueue(struct Queue *q) {
-
    if (q->begin == q->end)
       return EMPTY;
    if ((q->end + 1) % q->qsize == q->begin)
@@ -68,30 +41,16 @@ unsigned char StatusQueue(struct Queue *q) {
    return NORMAL;
 }
 
-
-/*
- * Enqueue a data in q
- */
 void EnQueue(struct Queue *q, unsigned int pos) {
-
    q->end = (q->end + 1) % q->qsize;
    q->memarea[q->end] = pos;
 }
 
-
-/*
- * Dequeue a data from q
- */
 void DeQueue(struct Queue *q, unsigned int *pos) {
-   
    q->begin = (q->begin + 1) % q->qsize;
    *pos = q->memarea[q->begin];
 }
 
-
-/*
- *
- */
 unsigned int Owner(unsigned int e, unsigned int statusarea) {
 
    if ((e == CHIP1 || e == CHIP2) && statusarea == NOTHING)
@@ -103,14 +62,8 @@ unsigned int Owner(unsigned int e, unsigned int statusarea) {
    return statusarea;
 }
 
-
-/*
- *
- */
 unsigned int Analyze(struct GOCell **goboard, unsigned int size, unsigned int x, unsigned int y, struct Queue *q, unsigned int statusarea) {
-
    unsigned int pos;
-
    if (goboard[x][y].statuscell == UNVISITED)
       if (goboard[x][y].element == NOCHIP) {
          pos = x * size + y;
@@ -121,14 +74,8 @@ unsigned int Analyze(struct GOCell **goboard, unsigned int size, unsigned int x,
    return statusarea;
 }
 
-
-/*
- *
- */
 int AnNeighb(struct GOCell **goboard, unsigned int size, struct Queue *q, unsigned int *lregion, unsigned int *statusarea) {
-
    unsigned int a, x, y, pos;
-
    a = 0;
    while (StatusQueue(q) != EMPTY) {
       DeQueue(q,&pos);
@@ -148,15 +95,11 @@ int AnNeighb(struct GOCell **goboard, unsigned int size, struct Queue *q, unsign
    return a;
 }
 
-
-/*
- *
- */
 void Process(struct GOCell **goboard, unsigned int n) {
 
    unsigned int i, j, a, pos, *lregion, x, y;
    unsigned int statusarea;
-   struct Queue *q;
+   struct QUEUE *q;
 
    q = InitQueue(n * n);
    lregion = calloc(n * n + 1,sizeof(unsigned int));
@@ -180,10 +123,6 @@ void Process(struct GOCell **goboard, unsigned int n) {
       }
 }
 
-
-/*
- *
- */
 void PrintGOBoard(struct GOCell **goboard, unsigned int n, char op) {
 
    unsigned int i, j;
@@ -201,19 +140,12 @@ void PrintGOBoard(struct GOCell **goboard, unsigned int n, char op) {
            if (goboard[i][j].element == CHIP2)
               printf("%c",CHIP2T);
         }
-     }
-     printf("\n");
+     } printf("\n");
    }
 }
 
-
-/*
- *
- */
 void ReadGOBoard(struct GOCell **goboard, unsigned int n) {
-
    unsigned int i, j;
-
    for (i = 1; i <= n; i = i + 1)
       for (j = 1; j <= n; j = j + 1) {
          scanf("%1d",&goboard[i][j].element);
@@ -221,12 +153,7 @@ void ReadGOBoard(struct GOCell **goboard, unsigned int n) {
       }
 }
 
-
-/*
- *
- */
 int main() {
-
    unsigned int n, i;
    struct Queue *q;
    struct GOCell **goboard;
