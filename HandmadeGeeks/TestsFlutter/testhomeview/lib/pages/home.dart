fcc77d1ed265a9812e7dda55c911844154652a05
AstroDolan <djaracarvajal@gmail.com>
2025-05-25 19:26:54 -0400

    Avances en la página de inicio

--- HandmadeGeeks/TestsFlutter/testhomeview/lib/pages/home.dart
+++ HandmadeGeeks/TestsFlutter/testhomeview/lib/pages/home.dart
@@ -7,56 +7,108 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Breakfast',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+      appBar: appBar(),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withValues(alpha: 0.1),
+                  blurRadius: 40,
+                  spreadRadius: 0.0, // changes position of shadow
+                ),
+              ],
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color(0xffF7F8F8),
+                contentPadding: EdgeInsets.all(15),
+                prefixIcon: Padding(
+                  //Que pasa si doy vuelta suffix y prefix??
+                  padding: const EdgeInsets.all(12.0),
+                  child: SvgPicture.asset('assets/icons/search.svg'),
+                ),
+                suffixIcon: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      VerticalDivider(
+                        color: Color(0xffDDDADA).withValues(alpha: 0.1),
+                        thickness: 0.1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SvgPicture.asset('assets/icons/filter.svg'),
+                      ),
+                    ],
+                  ),
+                ),
+                hintText: 'Search',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {
+          // Handle the tap event
+        },
+        child: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset('assets/icons/Arrow - Left2.svg'),
+          width: 20,
+          height: 20,
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10),
           ),
         ),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
-        leading: GestureDetector(
+      ),
+      actions: [
+        GestureDetector(
           onTap: () {
             // Handle the tap event
           },
           child: Container(
             margin: EdgeInsets.all(10),
             alignment: Alignment.center,
-            child: SvgPicture.asset('assets/icons/Arrow - Left2.svg'),
-            width: 20,
-            height: 20,
+            width: 37,
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              width: 20,
+              height: 20,
+            ),
             decoration: BoxDecoration(
               color: Color(0xffF7F8F8),
               borderRadius: BorderRadius.circular(10),
             ),
           ),
         ),
-        actions: [
-          GestureDetector(
-            onTap: () {
-              // Handle the tap event
-            },
-            child: Container(
-              margin: EdgeInsets.all(10),
-              alignment: Alignment.center,
-              width: 37,
-              child: SvgPicture.asset(
-                'assets/icons/dots.svg',
-                width: 20,
-                height: 20,
-              ),
-              decoration: BoxDecoration(
-                color: Color(0xffF7F8F8),
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-          ),
-        ],
-      ),
+      ],
     );
   }
 }
